ff98ec417adff9599f7329d88c54e90a2c16c0aa
GTMYang <289135816@qq.com>
2016-12-06 10:58:30 +0800

    Demo代码

---
+++ GTMActivityIndicatorViewDemo/Animations/NVActivityIndicatorAnimationBlank.swift
@@ -0,0 +1,17 @@
+//
+//  NVActivityIndicatorAnimationBlank.swift
+//  NVActivityIndicatorViewDemo
+//
+//  Created by Nguyen Vinh on 7/23/15.
+//  Copyright (c) 2015 Nguyen Vinh. All rights reserved.
+//
+
+import UIKit
+import GTMActivityIndicatorView
+
+class NVActivityIndicatorAnimationBlank: NVActivityIndicatorAnimation {
+    
+    func setUpAnimation(in layer: CALayer, size: CGSize, color: UIColor) {
+        // Do nothing
+    }
+}
