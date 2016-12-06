7d1bea014b1b9ee1e34c2e176de10ae73038e2ee
GTMYang <289135816@qq.com>
2016-12-06 09:56:45 +0800

    Initial Commit

---
+++ GTMActivityIndicatorView/GTMActivityIndicatorView.h
@@ -0,0 +1,19 @@
+//
+//  GTMActivityIndicatorView.h
+//  GTMActivityIndicatorView
+//
+//  Created by luoyang on 2016/12/6.
+//  Copyright © 2016年 syncsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GTMActivityIndicatorView.
+FOUNDATION_EXPORT double GTMActivityIndicatorViewVersionNumber;
+
+//! Project version string for GTMActivityIndicatorView.
+FOUNDATION_EXPORT const unsigned char GTMActivityIndicatorViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GTMActivityIndicatorView/PublicHeader.h>
+
+
